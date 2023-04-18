32800738a5e2aa047e346a3946009638085f9821
jonathandiazsavoy <jonathanmdiaz@hotmail.com>
2023-04-18 14:34:24 -0400

    Some wave logic updates

--- code/Managers/WaveManager.cs
+++ code/Managers/WaveManager.cs
@@ -1,15 +1,28 @@
+using code.Helpers;
 using Godot;
 
 public class WaveManager : YSort
 {
+    private GameManager gameManager;
     private YSort Enemies;
+    private YSort Items;
 
     public int EnemyCount { get; private set; }
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        gameManager = this.GetNode<GameManager>("/root/GameManager");
         Enemies = this.GetNode<YSort>("Enemies");
+        Items = this.GetNode<YSort>("Items");
         EnemyCount = Enemies.GetChildCount();
+    }
+
+    // **************************************************
+    // Signal listeners
+    // **************************************************
+    public void OnAlarmCountdownTimeout()
+    {
+        Items.QueueFree();
     }
 }
