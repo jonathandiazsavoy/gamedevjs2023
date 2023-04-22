e9a2001a775dbf5b4db4098661d6b32c6985bd1a
jonathandiazsavoy <jonathanmdiaz@hotmail.com>
2023-04-22 18:54:24 -0400

    Portal code updates

--- code/AreaTriggers/Portals/NextWavePortal.cs
+++ code/AreaTriggers/Portals/NextWavePortal.cs
@@ -2,8 +2,16 @@ using Godot;
 
 public class NextWavePortal : Area2D
 {
+    private GameManager gameManager;
+
     [Signal]
     public delegate void GoToNextWave(Player player);
+
+    public override void _Ready()
+    {
+        gameManager = this.GetNode<GameManager>(Master.NODE_PATH_TO_GAME_MANAGER);
+        this.Connect("GoToNextWave", gameManager, "OnGoToNextWave");
+    }
 
     public void OnPortalEntered(Node body)
     {
--- code/AreaTriggers/Portals/ShopPortal.cs
+++ code/AreaTriggers/Portals/ShopPortal.cs
@@ -2,8 +2,16 @@ using Godot;
 
 public class ShopPortal : Area2D
 {
+    private GameManager gameManager;
+
     [Signal]
     public delegate void GoToShop(Player player);
+
+    public override void _Ready()
+    {
+        gameManager = this.GetNode<GameManager>(Master.NODE_PATH_TO_GAME_MANAGER);
+        this.Connect("GoToShop", gameManager, "OnGoToShop");
+    }
 
     public void OnPortalEntered(Node body)
     {
